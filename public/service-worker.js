e25d32e1ed354f6af358cf00811b1dcc241f593f
Trent Roesbery <xtrent777x@gmail.com>
2021-10-14 21:13:51 -0600

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,89 @@
-//Module 19: Progressive Web Applications (PWA), Lesson 4: Using Service Workers as a refresher on how to add this to your application.
+const CACHE_NAME = 'offline-budget';
+const DATA_CACHE_NAME = 'offline-budget-v2';
+
+const FILES_TO_CACHE = [
+  "./", 
+  "./index.html",
+  "./manifest.json",
+  "./js/idb.js",
+  "./js/index.js",
+  "./css/style.css",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+];
+
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('your files were pre-cached successfully');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                  if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                      console.log('Removing old cache data', key);
+                      return caches.delete(key);
+                  }  
+                })
+            );
+        })
+    );
+    self.ClientRectList.claim();
+});
+
+
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(evt.request)
+                .then(response => {
+                  // If the response was good, clone it and store it in the cache.
+                  if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                  }
+    
+                  return response;
+                })
+                .catch(err => {
+                  // Network request failed, try to get it from the cache.
+                  return cache.match(evt.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+
+      evt.respondWith(
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get('accept').includes('text/html')) {
+              // return the cached home page for all requests for html pages
+              return caches.match('/');
+            }
+          });
+        })
+      );
+    
+});
+
+// module 19.4 and class activity 5
